61a859f2ddb08a1129fcacf90bf8e11e8c6820a5
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-19 23:45:50 +0800

    [backend] remove sections, merged into ui

--- apps/frontend/README.md
+++ apps/frontend/README.md
@@ -8,9 +8,6 @@
 ### Layouts
 - Layouts for the site
 - eg: `BaseLayout` has `Header` and child elements
-### Sections
-- Larger collation of UI that forms part of the site
-- eg: `Header` is the navbar
 ### UI
 - Smaller parts of the site
 - eg: buttons, styled containers
--- apps/frontend/src/components/ui/Header.jsx
+++ apps/frontend/src/components/ui/Header.jsx
@@ -1,5 +1,6 @@
 import { Heading, HStack, Button, Avatar } from "@chakra-ui/react"
 import Image from "next/image"
+import Link from "next/link"
 import bazaar_icon_alpha from "../../../public/bazaar_icon_alpha.png"
 
 const Header = () => {
@@ -10,14 +11,16 @@ const Header = () => {
       p={5}
       bgGradient="linear(to-r, #E5618D, #7667BB)"
     >
-      <HStack spacing={0}>
-        <Heading size="xl" color="black">
-          Bazaar
-        </Heading>
-        <div style={{ width: "30px", height: "30px" }}>
-          <Image src={bazaar_icon_alpha} alt="Bazaar Icon" />
-        </div>
-      </HStack>
+      <Link href="/" passHref>
+        <HStack spacing={0} cursor="pointer">
+          <Heading size="xl" color="black">
+            Bazaar
+          </Heading>
+          <div style={{ width: "30px", height: "30px" }}>
+            <Image src={bazaar_icon_alpha} alt="Bazaar Icon" />
+          </div>
+        </HStack>
+      </Link>
       <HStack justify="space-evenly" spacing={3}>
         <Button colorScheme="blackAlpha" variant="ghost" color="white">
           Marketplace
@@ -31,9 +34,11 @@ const Header = () => {
         <Button colorScheme="blackAlpha" variant="ghost" color="white">
           Albums
         </Button>
-        <Button colorScheme="blackAlpha" variant="ghost" color="white">
-          Login
-        </Button>
+        <Link href="/login" passHref>
+          <Button colorScheme="blackAlpha" variant="ghost" color="white">
+            Login
+          </Button>
+        </Link>
         <Avatar />
       </HStack>
     </HStack>
