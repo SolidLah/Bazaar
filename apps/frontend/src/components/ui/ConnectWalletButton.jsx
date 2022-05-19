a2ba85108b598f4f9ed2c04bbcdcdfd0b8f68408
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-19 23:49:20 +0800

    [backend] connect wallet button implementation with ethers.js

---
+++ apps/frontend/src/components/ui/ConnectWalletButton.jsx
@@ -0,0 +1,32 @@
+import { Button } from "@chakra-ui/react"
+import { useState, useEffect } from "react"
+import { ethers } from "ethers"
+
+const ConnectWalletButton = ({ currentAddress, setAddress }) => {
+  const [buttonText, updateButtonText] = useState("Connect Wallet")
+
+  useEffect(() => {
+    if (currentAddress) {
+      updateButtonText(currentAddress)
+    }
+  }, [currentAddress])
+
+  const connectWallet = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    await provider.send("eth_requestAccounts", [])
+    const signer = provider.getSigner()
+    const add = await signer.getAddress()
+
+    setAddress(add)
+  }
+
+  return (
+    <Button onClick={connectWallet}>
+      {buttonText === "Connect Wallet"
+        ? buttonText
+        : `${buttonText.slice(0, 3)}...${buttonText.slice(38)}`}
+    </Button>
+  )
+}
+
+export default ConnectWalletButton
--- apps/frontend/src/pages/index.jsx
+++ apps/frontend/src/pages/index.jsx
@@ -1,44 +1,19 @@
-import { Button, GridItem, SimpleGrid, Text } from "@chakra-ui/react"
+import { Center, Heading } from "@chakra-ui/react"
 import BaseLayout from "src/components/layouts/BaseLayout"
-import { useAccount, useConnect, useDisconnect } from "wagmi"
-import { MetaMaskConnector } from "wagmi/connectors/metaMask"
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import ConnectWalletButton from "src/components/ui/ConnectWalletButton"
 
 const Home = () => {
-  const { data: account, isLoading: accountLoading } = useAccount()
-  const { connect, isConnected } = useConnect()
-  const { disconnect } = useDisconnect()
+  const [currentAddress, setAddress] = useState("")
 
   return (
     <BaseLayout>
-      <SimpleGrid columns={2} columnGap={3} rowGap={3}>
-        <GridItem colSpan={2}>
-          <Text>Connect Account</Text>
-        </GridItem>
-        <GridItem colSpan={1}>
-          <Button
-            onClick={() => {
-              connect(new MetaMaskConnector())
-            }}
-          >
-            Connect
-          </Button>
-        </GridItem>
-        <GridItem colSpan={1}>
-          <Button
-            onClick={() => {
-              disconnect()
-            }}
-          >
-            Disconnect
-          </Button>
-        </GridItem>
-        <GridItem colSpan={2}></GridItem>
-        <Text>
-          Account:{" "}
-          {!accountLoading && isConnected ? account.address : "not connected"}
-        </Text>
-      </SimpleGrid>
+      <Center h="100vh" flexDirection="column">
+        <ConnectWalletButton
+          currentAddress={currentAddress}
+          setAddress={setAddress}
+        />
+      </Center>
     </BaseLayout>
   )
 }
