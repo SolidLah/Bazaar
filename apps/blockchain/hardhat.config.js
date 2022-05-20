655d31cd8d44d13877e94f34a5b76c7eb740b806
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-20 14:41:45 +0800

    [blockchain] comment out polygon settings first

--- apps/blockchain/hardhat.config.js
+++ apps/blockchain/hardhat.config.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-ethers")
 require("@nomiclabs/hardhat-etherscan")
+require("mocha")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,25 +20,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+// module.exports = {
+//   defaultNetwork: "matic",
+//   networks: {
+//     hardhat: {},
+//     matic: {
+//       url: "https://rpc-mumbai.maticvigil.com",
+//       accounts: [process.env.PRIVATE_KEY],
+//     },
+//   },
+//   etherscan: {
+//     apiKey: process.env.POLYGONSCAN_API_KEY,
+//   },
+//   solidity: {
+//     version: "0.8.4",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200,
+//       },
+//     },
+//   },
+// }
+
 module.exports = {
-  defaultNetwork: "matic",
-  networks: {
-    hardhat: {},
-    matic: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+  solidity: "0.8.4",
 }
