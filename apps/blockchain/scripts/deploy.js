8a55184d14120071ce545444dcbc24a8fa5f0441
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-21 15:44:25 +0800

    [blockchain] add contract address parameter for deploy script

--- apps/blockchain/scripts/deploy.js
+++ apps/blockchain/scripts/deploy.js
@@ -1,9 +1,13 @@
 const { ethers } = require("hardhat")
 
 async function main() {
-  const NFT = await ethers.getContractFactory("NFT")
-  const nft = await NFT.deploy("Bazaar NFT", "BFT")
+  const MARKETPLACE = await ethers.getContractFactory("Marketplace")
+  const marketplace = await MARKETPLACE.deploy(5)
 
+  const NFT = await ethers.getContractFactory("NFT")
+  const nft = await NFT.deploy("Bazaar NFT", "BFT", marketplace.address)
+
+  console.log("Marketplace contract address: ", marketplace.address)
   console.log("NFT contract address: ", nft.address)
 }
 
