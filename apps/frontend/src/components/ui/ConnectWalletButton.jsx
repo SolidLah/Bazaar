0b822472f6b4dab7579cef9a3d1e60d9764a40b6
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-23 21:24:29 +0800

    [frontend] abstract out ethers initialisation

--- apps/frontend/src/components/ui/ConnectWalletButton.jsx
+++ apps/frontend/src/components/ui/ConnectWalletButton.jsx
@@ -1,27 +1,14 @@
 import { Button } from "@chakra-ui/react"
-import { ethers } from "ethers"
+import { useState, useEffect } from "react"
 import { useWeb3Context } from "../../contexts/Web3Context"
 
 const ConnectWalletButton = () => {
   const web3Context = useWeb3Context()
-  const { currentAddress, setAddress } = web3Context.accounts
-  const { provider, setProvider, signer, setSigner } = web3Context.interface
+  const { initialiseEthers } = web3Context.functions
+  const { currentAddress } = web3Context.accounts
 
   const connectWallet = async () => {
-    if (typeof window.ethereum.isMetaMask !== undefined) {
-      const tmpProvider = provider
-        ? provider
-        : new ethers.providers.Web3Provider(window.ethereum)
-      await tmpProvider.send("eth_requestAccounts", [])
-      const tmpSigner = signer ? signer : tmpProvider.getSigner()
-      const address = await tmpSigner.getAddress()
-
-      setAddress(address)
-      setProvider(tmpProvider)
-      setSigner(tmpSigner)
-    } else {
-      console.log("MetaMask not detected")
-    }
+    await initialiseEthers()
   }
 
   return (
--- apps/frontend/src/contexts/Web3Context.jsx
+++ apps/frontend/src/contexts/Web3Context.jsx
@@ -1,6 +1,10 @@
+import { ethers } from "ethers"
 import { createContext, useContext, useState } from "react"
 
 const web3Context = createContext({
+  functions: {
+    initialiseEthers: null,
+  },
   contracts: {
     nft: {
       nftContract: null,
@@ -25,7 +29,37 @@ const Web3ContextProvider = ({ children }) => {
   const [currentAddress, setAddress] = useState("")
   const [nftContract, setNftContract] = useState(null)
 
+  const initialiseEthers = async () => {
+    if (typeof window.ethereum.isMetaMask === undefined) {
+      console.log("MetaMask not installed!")
+      return
+    }
+
+    try {
+      let tmpProvider = provider
+
+      if (!tmpProvider) {
+        tmpProvider = new ethers.providers.Web3Provider(window.ethereum)
+        setProvider(tmpProvider)
+
+        await tmpProvider.send("eth_requestAccounts", [])
+
+        const tmpSigner = tmpProvider.getSigner()
+        setSigner(tmpSigner)
+
+        const tmpAddress = await tmpSigner.getAddress()
+        setAddress(tmpAddress)
+      }
+    } catch (error) {
+      console.log(error)
+      return
+    }
+  }
+
   const values = {
+    functions: {
+      initialiseEthers: initialiseEthers,
+    },
     contracts: {
       nft: {
         nftContract: nftContract,
