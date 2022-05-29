b70c7d332a58205cfe82d618f4e62adb77cfa5dc
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-05-29 21:35:14 +0800

    [frontend] fix function name

--- apps/frontend/src/pages/reset.jsx
+++ apps/frontend/src/pages/reset.jsx
@@ -1,30 +1,29 @@
-import { auth, sendPasswordResetEmail } from "../../../backend/firebase";
+import { sendPasswordReset } from "../firebase"
 import { Button, Flex, Heading, Input } from "@chakra-ui/react"
 import Link from "next/link"
-import React, { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, { useState } from "react"
 
-const Reset = () => { 
-  const [email, setEmail] = useState("");
-  const [user, loading, error] = useAuthState(auth);
-  useEffect(() => {
-    if (loading) 
-        return;
-    if (user) 
-        alert("You are signed in!");;
-  }, [user, loading]);
+const Reset = () => {
+  const [email, setEmail] = useState("")
 
-return (
+  return (
     <Flex h="100vh" w="100vw" align="center" justify="center">
       <Flex direction="column" bg="gray.100" p={12} rounded="md">
         <Heading mb={6} align="center">
           Password Reset
         </Heading>
-        <Input placeholder="email" variant="filled" mb={3} 
+        <Input
+          placeholder="email"
+          variant="filled"
+          mb={3}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <Button colorScheme="teal" mb={6} onClick = {() => sendPasswordResetEmail(email)}>
+        <Button
+          colorScheme="teal"
+          mb={6}
+          onClick={() => sendPasswordReset(email)}
+        >
           Send Reset Link to Email
         </Button>
         <Link href="/login" passHref>
@@ -34,12 +33,14 @@ return (
         </Link>
         <Link href="/signup" passHref>
           <Button variant="link" size="sm">
-            {// eslint-disable-next-line react/no-unescaped-entities
-}           Don't have an account? Sign up with us now!
+            {
+              // eslint-disable-next-line react/no-unescaped-entities
+            }{" "}
+            Don't have an account? Sign up with us now!
           </Button>
         </Link>
       </Flex>
     </Flex>
   )
 }
-export default Reset;
+export default Reset
