a15db38be61e54bfa2160d7bb0a1991c01cf3e99
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-08 11:08:09 +0800

    [frontend] move Header into its own component

--- apps/frontend/src/pages/marketplace/index.jsx
+++ apps/frontend/src/pages/marketplace/index.jsx
@@ -1,22 +1,29 @@
-import { Button, Heading, VStack, HStack } from "@chakra-ui/react"
+import { Button, Heading, VStack, HStack, ButtonGroup } from "@chakra-ui/react"
 import Link from "next/link"
 import Carousel from "src/components/ui/Carousel"
+import stub from "../../stub"
+
+const Header = () => {
+  return (
+    <HStack w="100%" justify="space-between">
+      <Heading>Marketplace</Heading>
+      <ButtonGroup size="lg" colorScheme="teal" pr={10} gap={3}>
+        <Link href="/marketplace/mint" passHref>
+          <Button>Mint</Button>
+        </Link>
+        <Link href="/marketplace/mint" passHref>
+          <Button>List</Button>
+        </Link>
+      </ButtonGroup>
+    </HStack>
+  )
+}
 
 const Marketplace = () => {
   return (
-    <VStack p={10} spacing={20}>
-      <HStack w="100%" justify="space-between">
-        <Heading>Marketplace</Heading>
-        <HStack paddingRight={10} spacing={3}>
-          <Link href="/marketplace/mint" passHref>
-            <Button size="lg" colorScheme="teal">Mint</Button>
-          </Link>
-          <Link href="/marketplace/mint" passHref>
-            <Button size="lg" colorScheme="teal">List</Button>
-          </Link>
-        </HStack>
-      </HStack>
-      <Carousel />
+    <VStack w="100%" p={10} spacing={20}>
+      <Header />
+      <Carousel items={stub} />
     </VStack>
   )
 }
