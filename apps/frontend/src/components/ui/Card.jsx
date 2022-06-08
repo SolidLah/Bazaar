c5060176521a8f62333fe8d2b347ef1b68d87fda
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-08 15:37:53 +0800

    [frontend] hover interaction for Carousel Card

--- apps/frontend/src/components/ui/Card.jsx
+++ apps/frontend/src/components/ui/Card.jsx
@@ -1,10 +1,17 @@
-import { Square, Flex, Box, Text } from "@chakra-ui/react"
+import { Square, Flex, Box, Text, Heading } from "@chakra-ui/react"
 import Image from "next/image"
 import bazaar_icon_alpha from "../../../public/bazaar_icon_alpha.png"
 
 const Card = ({ item }) => {
   return (
-    <Square size={60} bg="gray.100" m={3} borderRadius="lg">
+    <Square
+      size={60}
+      bg="gray.100"
+      m={3}
+      borderRadius="lg"
+      _hover={{ shadow: "md" }}
+      cursor="pointer"
+    >
       <Flex
         w="100%"
         h="100%"
@@ -14,7 +21,7 @@ const Card = ({ item }) => {
       >
         <Image src={bazaar_icon_alpha} alt="Bazaar Icon" />
         <Box bg="gray.200" w="100%" p={3} borderBottomRadius="lg">
-          <Text>{item.name}</Text>
+          <Heading size="sm">{item.name}</Heading>
           <Text>{item.description}</Text>
         </Box>
       </Flex>
