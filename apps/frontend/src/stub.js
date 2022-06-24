96e1d90eb173f420f90edbb316e94aa359b9f3e1
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-25 01:05:09 +0800

    [frontend] new format for stub data

--- apps/frontend/src/stub.js
+++ apps/frontend/src/stub.js
@@ -1,46 +1,128 @@
-const items = [
-  {
-    name: "NFT 1",
-    description: "description 1",
-    image:
-      "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-    price: "1",
-  },
-  {
-    name: "NFT 2",
-    description: "description 2",
-    image:
-      "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-    price: "1",
-  },
-  {
-    name: "NFT 3",
-    description: "description 3",
-    image:
-      "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-    price: "1",
-  },
-  {
-    name: "NFT 4",
-    description: "description 4",
-    image:
-      "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-    price: "1",
-  },
-  {
-    name: "NFT 5",
-    description: "description 5",
-    image:
-      "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-    price: "1",
-  },
-  {
-    name: "NFT 6",
-    description: "description 6",
-    image:
-      "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-    price: "1",
-  },
-]
+const items = {
+  userListings: [
+    {
+      id: 1,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 2,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 3,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 4,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 5,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 6,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+  ],
+  userNFTs: [
+    {
+      id: 1,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 2,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 3,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 4,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 5,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+    {
+      id: 6,
+      nftData: {
+        image: "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+        name: "stub",
+      },
+      marketPrice: {
+        display: 10,
+      },
+    },
+  ],
+}
 
 export default items
