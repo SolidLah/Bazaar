0e532d7463ffa6633b8122201417a0cbefb2d81f
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-25 01:06:11 +0800

    [frontend] change url links for navbar for new user pages structure

--- apps/frontend/src/components/ui/Navbar.jsx
+++ apps/frontend/src/components/ui/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   const logoutCallback = async () => {
     await logout()
-    router.push("/login")
+    router.push("/user/login")
   }
 
   return (
@@ -59,13 +59,13 @@ const Navbar = () => {
             Logout
           </Button>
         ) : (
-          <Link href="/login" passHref>
+          <Link href="/user/login" passHref>
             <Button colorScheme="blackAlpha" variant="ghost" color="white">
               Login
             </Button>
           </Link>
         )}
-        <Link href="/me" passHref>
+        <Link href="/user/me" passHref>
           <Avatar cursor="pointer" />
         </Link>
       </HStack>
