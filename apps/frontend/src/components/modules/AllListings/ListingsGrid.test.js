75ee39cd701adea6a5fcdba78c6297f82f873e33
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-30 01:21:16 +0800

    [frontend] added ListingsGrid test

---
+++ apps/frontend/src/components/modules/AllListings/ListingsGrid.test.js
@@ -0,0 +1,46 @@
+import ListingsGrid from "./ListingsGrid";
+import { render, screen } from "@testing-library/react";
+
+const stubItems = [
+  {
+    id: 1,
+    nftData: {
+      image:
+        "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+      name: "stub",
+    },
+    marketPrice: {
+      display: 10,
+    },
+  },
+  {
+    id: 2,
+    nftData: {
+      image:
+        "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+      name: "stub",
+    },
+    marketPrice: {
+      display: 10,
+    },
+  },
+  {
+    id: 3,
+    nftData: {
+      image:
+        "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+      name: "stub",
+    },
+    marketPrice: {
+      display: 10,
+    },
+  },
+];
+
+describe("ListingsGrid component", () => {
+  it("Displays market items correctly", () => {
+    render(<ListingsGrid items={stubItems} />);
+    const numberOfCards = screen.getAllByText("stub");
+    expect(numberOfCards).toHaveLength(3);
+  });
+});
