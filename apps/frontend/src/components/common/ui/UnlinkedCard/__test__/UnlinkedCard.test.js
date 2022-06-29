a2e258451ad1e55026598d26cf910648e3245fd1
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-29 16:16:23 +0800

    [frontend] skip navbar test for now, added unlinkedcard test

---
+++ apps/frontend/src/components/common/ui/UnlinkedCard/__test__/UnlinkedCard.test.js
@@ -0,0 +1,16 @@
+import UnlinkedCard from "../UnlinkedCard";
+import { render, screen } from "@testing-library/react";
+
+const stubItem = {
+  image:
+    "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
+  name: "stub",
+};
+
+describe("UnlinkedCard component", () => {
+  it("NFT name is displayed in UnlinkedCard component", () => {
+    render(<UnlinkedCard item={stubItem} />);
+    const nftName = screen.getByText(stubItem.name, { exact: false });
+    expect(nftName).toBeInTheDocument();
+  });
+});
