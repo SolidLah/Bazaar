7a978cec38ecd859f28ebace52958853a28be2a3
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-06-29 14:08:47 +0800

    [frontend] remove console log in test

--- apps/frontend/src/components/common/ui/Card/__test__/Card.test.js
+++ apps/frontend/src/components/common/ui/Card/__test__/Card.test.js
@@ -9,7 +9,6 @@ describe("Card component", () => {
   it("Props are passed correctly to Card component", () => {
     render(<Card item={userItem} />);
     const nftName = screen.getByText(userItem.nftData.name);
-    console.log(nftName);
     expect(nftName).toBeInTheDocument();
   });
 });
