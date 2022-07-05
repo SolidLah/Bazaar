bdcbd2711b5ac0f10a4e308902455e03bf2fb78b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-05 10:12:47 +0800

    [frontend] update stub data to be the new item format

--- apps/frontend/src/lib/stubData.js
+++ apps/frontend/src/lib/stubData.js
@@ -1,83 +1,43 @@
 const stubItems = {
   userListings: [
     {
-      id: 1,
+      itemId: 1,
       nftData: {
         image:
           "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
         name: "stub-listing",
       },
-      marketPrice: {
-        display: 10,
-      },
+      marketPrice: 10,
     },
     {
-      id: 2,
+      itemId: 2,
       nftData: {
         image:
           "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
         name: "stub-listing",
       },
-      marketPrice: {
-        display: 10,
-      },
+      marketPrice: 10,
     },
     {
-      id: 3,
+      itemId: 3,
       nftData: {
         image:
           "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
         name: "stub-listing",
       },
-      marketPrice: {
-        display: 10,
-      },
+      marketPrice: 10,
     },
     {
-      id: 4,
+      itemId: 4,
       nftData: {
         image:
           "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
         name: "stub-listing",
       },
-      marketPrice: {
-        display: 10,
-      },
-    },
-    {
-      id: 5,
-      nftData: {
-        image:
-          "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-        name: "stub-listing",
-      },
-      marketPrice: {
-        display: 10,
-      },
-    },
-    {
-      id: 6,
-      nftData: {
-        image:
-          "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-        name: "stub-listing",
-      },
-      marketPrice: {
-        display: 10,
-      },
+      marketPrice: 10,
     },
   ],
   userNFTs: [
-    {
-      image:
-        "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-      name: "stub-owned",
-    },
-    {
-      image:
-        "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
-      name: "stub-owned",
-    },
     {
       image:
         "https://gateway.pinata.cloud/ipfs/QmddQwKp51S5JXC4wiNEZxYLBkQjq3L3k9UizWAj3RqSAd",
