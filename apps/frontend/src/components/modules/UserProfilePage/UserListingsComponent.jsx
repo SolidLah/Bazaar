26327b632cdadde21f279c86176a14a1a914dd39
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-10 01:40:21 +0800

    [frontend] handle empty props

--- apps/frontend/src/components/modules/UserProfilePage/UserListingsComponent.jsx
+++ apps/frontend/src/components/modules/UserProfilePage/UserListingsComponent.jsx
@@ -2,6 +2,10 @@ import { Center, Flex } from "@chakra-ui/react";
 import LinkedCard from "src/components/common/ui/LinkedCard/LinkedCard";
 
 const UserListingsComponent = ({ listed }) => {
+  if (!listed) {
+    return <Center>No listings</Center>;
+  }
+
   return listed?.length > 0 ? (
     <Flex wrap="wrap" justify="flex-start">
       {listed.map((item) => (
--- apps/frontend/src/components/modules/UserProfilePage/UserOwnedComponent.jsx
+++ apps/frontend/src/components/modules/UserProfilePage/UserOwnedComponent.jsx
@@ -2,6 +2,10 @@ import { Center, Flex } from "@chakra-ui/react";
 import Card from "src/components/common/ui/Card/Card";
 
 const UserOwnedComponent = ({ owned }) => {
+  if (!owned) {
+    return <Center>No NFTs owned</Center>;
+  }
+
   return owned?.length > 0 ? (
     <Flex wrap="wrap" justify="flex-start">
       {owned.map((item) => (
