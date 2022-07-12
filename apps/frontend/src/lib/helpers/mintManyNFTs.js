5cd696f8a11fcaaa6ad286102b926fad75cb87f6
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-12 22:22:08 +0800

    [frontend] dont return tokenIds

--- apps/frontend/src/lib/helpers/mintManyNFTs.js
+++ apps/frontend/src/lib/helpers/mintManyNFTs.js
@@ -2,12 +2,9 @@ import { ethers } from "ethers";
 import { NFTContractData } from "src/contracts";
 import useEthersStore from "src/stores/ethersStore";
 
-export default async function mintManyNFTs (address, urls) {
+export default async function mintManyNFTs(address, urls) {
   const signer = useEthersStore.getState().signer;
   const nftContract = new ethers.Contract(address, NFTContractData.abi, signer);
 
-  await (await nftContract.mintMany(urls)).wait(); // mint NFTs
-  const tokenId = await nftContract.idCounter(); // get tokenIds
-
-  return tokenId;
+  await (await nftContract.mintMany(urls)).wait();
 }
--- apps/frontend/src/lib/helpers/newMintNFT.js
+++ apps/frontend/src/lib/helpers/newMintNFT.js
@@ -6,8 +6,5 @@ export default async function newMintNFT(address, url) {
   const signer = useEthersStore.getState().signer;
   const nftContract = new ethers.Contract(address, NFTContractData.abi, signer);
 
-  await (await nftContract.mint(url)).wait(); // mint NFT
-  const tokenId = await nftContract.idCounter(); // get tokenId
-
-  return tokenId;
+  await (await nftContract.mint(url)).wait();
 }
