723f953f978240aa9de024f2da8cafcfa83544ca
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-14 18:51:14 +0800

    [frontend] update contract info

--- apps/frontend/src/lib/helpers/mintManyNFTs.js
+++ apps/frontend/src/lib/helpers/mintManyNFTs.js
@@ -1,10 +1,24 @@
 import { ethers } from "ethers";
-import { NFTContractData } from "src/contracts";
+import { MarketplaceContractData, NFTContractData } from "src/contracts";
 import useEthersStore from "src/stores/ethersStore";
 
 export default async function mintManyNFTs(address, urls) {
   const signer = useEthersStore.getState().signer;
   const nftContract = new ethers.Contract(address, NFTContractData.abi, signer);
+  const mktContract = new ethers.Contract(
+    MarketplaceContractData.address,
+    MarketplaceContractData.abi,
+    signer
+  );
 
+  const beforeTokenId = await nftContract.idCounter();
   await (await nftContract.mintMany(urls)).wait();
+  const afterTokenId = await nftContract.idCounter();
+
+  let tokenIds = [];
+  for (i = 1; i <= afterTokenId; i++) {
+    tokenIds.push(i + beforeTokenId);
+  }
+
+  await (await mktContract.createManyMarketItems(tokenIds)).wait();
 }
--- apps/frontend/src/lib/helpers/mintNFT.js
+++ apps/frontend/src/lib/helpers/mintNFT.js
@@ -1,10 +1,17 @@
+import { ethers } from "ethers";
+import { MarketplaceContractData, NFTContractData } from "src/contracts";
 import useEthersStore from "src/stores/ethersStore";
 
-export default async function mintNFT(nftURI) {
-  const nftContract = useEthersStore.getState().nftContract;
+export default async function mintNFT(address, url) {
+  const signer = useEthersStore.getState().signer;
+  const nftContract = new ethers.Contract(address, NFTContractData.abi, signer);
+  const mktContract = new ethers.Contract(
+    MarketplaceContractData.address,
+    MarketplaceContractData.abi,
+    signer
+  );
 
-  await (await nftContract.mint(nftURI)).wait(); // mint NFT
-  const tokenId = await nftContract.idCounter(); // get tokenId
-
-  return tokenId;
+  await (await nftContract.mint(url)).wait();
+  const tokenId = await nftContract.idCounter();
+  await (await mktContract.createMarketItem(tokenId)).wait();
 }
