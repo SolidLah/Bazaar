9b27fb8f5eaaf18de226e282a142cee1859e142b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-15 16:07:36 +0800

    [frontend] get relevant params

--- apps/frontend/src/lib/helpers/listNFT.js
+++ apps/frontend/src/lib/helpers/listNFT.js
@@ -3,7 +3,8 @@ import { NFTContractData } from "src/contracts";
 import useEthersStore from "src/stores/ethersStore";
 import toWei from "./toWei";
 
-export default async function listNFT(itemId, price) {
+export default async function listNFT(address, itemId, price) {
+  const signer = useEthersStore.getState().signer;
   const mktContract = useEthersStore.getState().mktContract;
   const nftContract = new ethers.Contract(address, NFTContractData.abi, signer);
 
