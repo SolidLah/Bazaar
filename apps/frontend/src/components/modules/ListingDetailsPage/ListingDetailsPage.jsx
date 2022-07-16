19914c85c57f05d50f6252cf600f7951f9908870
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-16 17:27:54 +0800

    [frontend] cut out one layer

--- apps/frontend/src/components/modules/ListingDetailsPage/ListingDetailsPage.jsx
+++ apps/frontend/src/components/modules/ListingDetailsPage/ListingDetailsPage.jsx
@@ -1,12 +1,17 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
 import axios from "axios";
+import Image from "next/image";
 import { useMemo } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import ErrorLayout from "src/components/common/layouts/ErrorLayout";
 import LoadingLayout from "src/components/common/layouts/LoadingLayout";
+import { blurImage } from "src/lib/blurImage";
 import { auth } from "src/lib/firebase";
 import useEthersStore from "src/stores/ethersStore";
 import useSWR from "swr";
-import ListingDetailsElement from "./ListingDetailsElement";
+import BuyComponent from "./BuyComponent";
+import DetailsCard from "./DetailsCard";
+import ListComponent from "./ListComponent";
 
 const ListingDetailsPage = ({ id }) => {
   const [user] = useAuthState(auth);
@@ -26,12 +31,28 @@ const ListingDetailsPage = ({ id }) => {
   }
 
   return (
-    <ListingDetailsElement
-      user={user}
-      item={item}
-      active={active}
-      walletAddress={walletAddress}
-    />
+    <Container centerContent maxW="container.xl" mt={20}>
+      <Flex w="100%" justify="space-around" align="flex-start">
+        <Box w="xl" h="xl" pos="relative">
+          <Image
+            src={item.nftData.image}
+            alt="NFT here"
+            layout="fill"
+            objectFit="contain"
+            placeholder="blur"
+            blurDataURL={blurImage}
+          />
+        </Box>
+        <Flex w="md" h="100%" direction="column" justify="flex-start" gap={6}>
+          <DetailsCard item={item} active={active} />
+          {active ? (
+            <BuyComponent item={item} user={user} />
+          ) : (
+            <ListComponent item={item} walletAddress={walletAddress} />
+          )}
+        </Flex>
+      </Flex>
+    </Container>
   );
 };
 
