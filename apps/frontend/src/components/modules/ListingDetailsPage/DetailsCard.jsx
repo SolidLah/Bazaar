19bd9ad35ea4ac8b1f058b6757f7aae125a21bb7
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-17 22:53:58 +0800

    [frontend] link collection name to collection page in listing details page

--- apps/frontend/src/components/modules/ListingDetailsPage/DetailsCard.jsx
+++ apps/frontend/src/components/modules/ListingDetailsPage/DetailsCard.jsx
@@ -1,4 +1,5 @@
-import { Flex, Heading, Text, Badge } from "@chakra-ui/react";
+import { Flex, Heading, Text, Badge, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { formatAddress } from "src/lib/helpers";
 
 const DetailsCard = ({ item, active }) => {
@@ -6,7 +7,9 @@ const DetailsCard = ({ item, active }) => {
     <Flex w="md" bg="gray.100" rounded="md" p={3}>
       <Flex w="100%" direction="column">
         <Heading>{item.nftData.name}</Heading>
-        <Text>{`${item.collectionName} (${item.collectionSymbol})`}</Text>
+        <NextLink href={`/collection/${item.collectionAddress}`} passHref>
+          <Link color="purple">{`${item.collectionName} (${item.collectionSymbol})`}</Link>
+        </NextLink>
         <Text>{formatAddress(item.minter)}</Text>
         <Text mt={6} as="i">
           {item.nftData.description}
