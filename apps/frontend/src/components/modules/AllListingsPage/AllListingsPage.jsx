bd49269d2fb6ddb797276a2d15f9fdd166509f26
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-17 13:27:51 +0800

    [frontend] added search functionality to marketplace

--- apps/frontend/src/components/modules/AllListingsPage/AllListingsPage.jsx
+++ apps/frontend/src/components/modules/AllListingsPage/AllListingsPage.jsx
@@ -4,13 +4,26 @@ import useSWR from "swr";
 import ErrorLayout from "src/components/common/layouts/ErrorLayout";
 import Header from "./Header";
 import ListingsGrid from "./ListingsGrid";
+import { useMemo, useState } from "react";
 
 const AllListingsPage = () => {
-  const { data, error } = useSWR(
+  const { data: items, error } = useSWR(
     "/api/listings",
     (url) => axios.get(url).then((res) => res.data.msg),
     { revalidateOnFocus: false }
   );
+
+  const [query, setQuery] = useState("");
+  const queriedItems = useMemo(() => {
+    if (!query || !items) return items;
+
+    const capsQuery = query.toUpperCase();
+    const res = items.filter((item) => {
+      const capsName = item.nftData.name.toUpperCase();
+      return capsName.includes(capsQuery);
+    });
+    return res;
+  }, [query, items]);
 
   if (error) {
     return <ErrorLayout />;
@@ -18,11 +31,11 @@ const AllListingsPage = () => {
 
   return (
     <Container maxW="container.xl" centerContent gap={6} mt={20}>
-      <Header />
-      {!data ? (
+      <Header query={query} setQuery={setQuery} />
+      {!queriedItems ? (
         <Spinner size="xl" color="gray" />
       ) : (
-        <ListingsGrid items={data} />
+        <ListingsGrid items={queriedItems} />
       )}
     </Container>
   );
--- apps/frontend/src/components/modules/AllListingsPage/Header.jsx
+++ apps/frontend/src/components/modules/AllListingsPage/Header.jsx
@@ -1,18 +1,45 @@
-import { Button, Flex, Heading } from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
 import Link from "next/link";
 
-const Header = () => {
+const Header = ({ query, setQuery }) => {
+  const searchInputHandler = (e) => {
+    e.preventDefault();
+    setQuery(e.target.value);
+  };
+
   return (
     <Flex
       w="100%"
-      justify="space-between"
       mx={10}
       bg="gray.100"
       p={6}
       rounded="md"
-      align="center"
+      align="flex-end"
+      justify="space-between"
     >
       <Heading>Marketplace</Heading>
+      <Flex>
+        <InputGroup>
+          <InputLeftElement>
+            <Search2Icon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            w="xs"
+            variant="flushed"
+            placeholder="Search"
+            value={query ?? null}
+            onChange={searchInputHandler}
+          />
+        </InputGroup>
+      </Flex>
       <Link href="/collection/new" passHref>
         <Button as="a" size="lg" colorScheme="purple">
           New Collection
