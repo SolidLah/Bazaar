9a1631d206ea2575b925253349f4043a2b60f282
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-17 15:05:41 +0800

    [frontend] handle non int inputs

--- apps/frontend/src/components/modules/AllListingsPage/useQueryByPrice.js
+++ apps/frontend/src/components/modules/AllListingsPage/useQueryByPrice.js
@@ -2,13 +2,17 @@ import { useMemo } from "react";
 
 export default function useQueryByPrice(range, items) {
   return useMemo(() => {
-    if ((!range.lower && !range.upper) || !items) return items;
+    const lower = ~~range.lower;
+    const upper = ~~range.upper;
+
+    if ((!lower && !upper) || !items) return items;
 
     const res = items.filter((item) => {
-      const itemPrice = item.marketPrice;
-      if (!range.lower && range.upper) return itemPrice <= range.upper;
-      if (range.lower && !range.upper) return range.lower <= itemPrice;
-      if (range.lower && range.upper)
+      const itemPrice = ~~item.marketPrice;
+
+      if (!lower && upper) return itemPrice <= range.upper;
+      if (lower && !upper) return range.lower <= itemPrice;
+      if (lower && upper)
         return range.lower <= itemPrice && itemPrice <= range.upper;
     });
     return res;
