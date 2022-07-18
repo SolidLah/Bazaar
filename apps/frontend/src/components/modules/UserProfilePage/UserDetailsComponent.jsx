a8234efeaf9b9a443e8d714db251fdb71ada47f4
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-18 19:38:50 +0800

    [frontend] data by uid

--- apps/frontend/src/components/modules/UserProfilePage/UserDetailsComponent.jsx
+++ apps/frontend/src/components/modules/UserProfilePage/UserDetailsComponent.jsx
@@ -2,35 +2,21 @@ import { Text, Avatar, Button, Flex, Box } from "@chakra-ui/react";
 import useEthersStore from "src/stores/ethersStore";
 import useErrorToast from "src/lib/hooks/useErrorToast";
 import useSuccessToast from "src/lib/hooks/useSuccessToast";
-import { formatAddress, updateWalletAddress } from "src/lib/helpers";
+import { formatAddress } from "src/lib/helpers";
 import { useStoredAddress } from "src/lib/hooks";
+import useName from "src/lib/hooks/useName";
+import useEmail from "src/lib/hooks/useEmail";
 
-const UserDetailsComponent = ({ user, userData }) => {
+const UserDetailsComponent = ({ userData }) => {
   const ethersInitialised = useEthersStore((state) => state.ethersInitialised);
-  const ethersStoredAddress = useEthersStore((state) => state.address);
+  const ethersAddress = useEthersStore((state) => state.address);
   const errorToast = useErrorToast("Connect wallet to account");
   const successToast = useSuccessToast("Connect wallet to account");
   const storedAddress = useStoredAddress(userData);
+  const name = useName(userData);
+  const email = useEmail(userData);
 
   const buttonCallback = async () => {
-    if (!ethersInitialised) {
-      errorToast({
-        description: "No connected wallet found",
-      });
-
-      return;
-    }
-
-    if (ethersStoredAddress === storedAddress) {
-      errorToast({
-        description: "No change in wallet address",
-      });
-
-      return;
-    }
-
-    await updateWalletAddress(user.uid, ethersStoredAddress);
-
     successToast({
       description: "Wallet has been connected successfully",
     });
@@ -52,11 +38,11 @@ const UserDetailsComponent = ({ user, userData }) => {
       <Flex direction="column" gap={3} w="100%">
         <Box>
           <Text fontWeight="bold">Name</Text>
-          <Text>{user.displayName}</Text>
+          <Text>{name}</Text>
         </Box>
         <Box>
           <Text fontWeight="bold">Email</Text>
-          <Text>{user.email}</Text>
+          <Text>{email}</Text>
         </Box>
         <Box>
           <Text fontWeight="bold">Wallet address</Text>
