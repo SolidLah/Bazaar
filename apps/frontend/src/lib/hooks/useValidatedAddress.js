767e993e6cd38563dd7bbbd31d380d9c2848f9a9
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-19 22:57:58 +0800

    [frontend] use errorToasts in validation function

--- apps/frontend/src/lib/hooks/useValidatedAddress.js
+++ apps/frontend/src/lib/hooks/useValidatedAddress.js
@@ -1,8 +1,12 @@
-import { useContext, useMemo } from "react";
+import { useContext, useState } from "react";
 import { userContext } from "src/contexts/userContext";
 import useEthersStore from "src/stores/ethersStore";
+import useErrorToast from "./useErrorToast";
 
 export default function useValidatedAddress() {
+  const errorToast = useErrorToast("Wallet address");
+  const [isValidated, setValidated] = useState(false);
+
   // firestore address
   const { authState, firestoreHook } = useContext(userContext);
   const [user] = authState;
@@ -13,10 +17,39 @@ export default function useValidatedAddress() {
   const metamaskAddress = useEthersStore((state) => state.address);
 
   // validation
-  const isValidated = useMemo(() => {
-    if (!user) return false;
-    return storedAddress === metamaskAddress;
-  }, [user, storedAddress, metamaskAddress]);
+  const validateAddress = () => {
+    setValidated(false);
 
-  return isValidated;
+    if (!user) {
+      errorToast({
+        description: "Not logged in",
+      });
+      return;
+    }
+
+    if (!storedAddress) {
+      errorToast({
+        description: "No wallet address associated with this user",
+      });
+      return;
+    }
+
+    if (!metamaskAddress) {
+      errorToast({
+        description: "Metamask not connected",
+      });
+      return;
+    }
+
+    if (storedAddress !== metamaskAddress) {
+      errorToast({
+        description: "Current metamask wallet does not match user's wallet",
+      });
+      return;
+    }
+
+    setValidated(true);
+  };
+
+  return { isValidated, validateAddress };
 }
