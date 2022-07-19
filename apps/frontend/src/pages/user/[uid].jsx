9c290bb23f938dbb22282f6673179630c7f3055f
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-19 21:23:56 +0800

    [frontend] protect routes

--- apps/frontend/src/pages/user/[uid].jsx
+++ apps/frontend/src/pages/user/[uid].jsx
@@ -1,11 +1,16 @@
 import { useRouter } from "next/router";
+import ProtectedRouteLayout from "src/components/common/layouts/ProtectedRouteLayout";
 import UserProfilePage from "src/components/modules/UserProfilePage/UserProfilePage";
 
 const Details = () => {
   const router = useRouter();
   const { uid } = router.query;
 
-  return <UserProfilePage uid={uid} />;
+  return (
+    <ProtectedRouteLayout>
+      <UserProfilePage uid={uid} />
+    </ProtectedRouteLayout>
+  );
 };
 
 export default Details;
