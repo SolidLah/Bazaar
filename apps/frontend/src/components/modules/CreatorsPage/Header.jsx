c69b732f7c4d9762ce1b8339b41b64171f5c24de
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-20 13:00:50 +0800

    [frontend] check if user logged in before rendering sign up button

--- apps/frontend/src/components/modules/CreatorsPage/Header.jsx
+++ apps/frontend/src/components/modules/CreatorsPage/Header.jsx
@@ -1,6 +1,12 @@
 import { Button, ButtonGroup, Flex, Heading } from "@chakra-ui/react";
+import Link from "next/link";
+import { useContext } from "react";
+import { userContext } from "src/contexts/userContext";
 
 const Header = () => {
+  const { authState } = useContext(userContext);
+  const [user] = authState;
+
   return (
     <Flex
       w="100%"
@@ -12,8 +18,14 @@ const Header = () => {
     >
       <Heading>Creators</Heading>
       <ButtonGroup colorScheme="purple" size="lg">
-        <Button>Sign up</Button>
-        <Button>New collection</Button>
+        {!user && (
+          <Link href="/user/signup/" passHref>
+            <Button as="a">Sign Up</Button>
+          </Link>
+        )}
+        <Link href="/collection/new" passHref>
+          <Button as="a">New Collection</Button>
+        </Link>
       </ButtonGroup>
     </Flex>
   );
