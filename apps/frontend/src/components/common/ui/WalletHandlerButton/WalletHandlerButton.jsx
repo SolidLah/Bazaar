a22715a1b053b66baef52f4d62be562672936137
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-22 19:34:01 +0800

    [frontend] use toastedCallback hook

--- apps/frontend/src/components/common/ui/WalletHandlerButton/WalletHandlerButton.jsx
+++ apps/frontend/src/components/common/ui/WalletHandlerButton/WalletHandlerButton.jsx
@@ -1,58 +1,38 @@
 import { Button } from "@chakra-ui/react";
 import useEthersStore from "src/stores/ethersStore";
-import useErrorToast from "src/lib/hooks/useErrorToast";
 import useSuccessToast from "src/lib/hooks/useSuccessToast";
-import { useState } from "react";
 import { formatAddress } from "src/lib/helpers";
+import { useToastedCallback } from "src/lib/hooks";
 
 const WalletHandlerButton = (props) => {
   const address = useEthersStore((state) => state.address);
   const connectEthers = useEthersStore((state) => state.connectEthers);
   const disconnectEthers = useEthersStore((state) => state.disconnectEthers);
   const ethersInitialised = useEthersStore((state) => state.ethersInitialised);
-  const errorToast = useErrorToast("Connect wallet");
-  const successToast = useSuccessToast("Connect wallet");
+  const successToast = useSuccessToast("Handle wallet");
 
-  const [loading, setLoading] = useState(false);
-
-  const connect = async () => {
-    try {
-      setLoading(true);
-      await connectEthers();
-      successToast({
-        description: "Wallet connected successfully",
-      });
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      errorToast({
-        description: error.message,
-      });
-    }
-  };
-
-  const disconnect = async () => {
-    try {
-      setLoading(true);
+  const callBack = async () => {
+    if (ethersInitialised) {
       await disconnectEthers();
       successToast({
         description: "Wallet disconnected successfully",
       });
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      errorToast({
-        description: error.message,
+    } else {
+      await connectEthers();
+      successToast({
+        description: "Wallet connected successfully",
       });
     }
   };
 
+  const { toastedCallback, loading } = useToastedCallback(
+    "Handle wallet",
+    callBack,
+    false
+  );
+
   return (
-    <Button
-      onClick={ethersInitialised ? disconnect : connect}
-      isLoading={loading}
-      {...props}
-    >
+    <Button onClick={toastedCallback} isLoading={loading} {...props}>
       {address ? formatAddress(address) : "Connect Wallet"}
     </Button>
   );
