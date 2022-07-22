c0aa148272f8782826102dd20dc0caa42417c95a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-22 10:58:47 +0800

    [frontend] display background image

--- apps/frontend/src/components/modules/UserProfilePage/UserProfilePage.jsx
+++ apps/frontend/src/components/modules/UserProfilePage/UserProfilePage.jsx
@@ -1,4 +1,4 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex } from "@chakra-ui/react";
 import ErrorLayout from "src/components/common/layouts/ErrorLayout";
 import LoadingLayout from "src/components/common/layouts/LoadingLayout";
 import useFirestoreUidData from "src/lib/hooks/useFirestoreUidData";
@@ -17,12 +17,21 @@ const UserProfilePage = ({ uid }) => {
   }
 
   return (
-    <Container maxW="container.xl" mt={20}>
-      <Flex gap={6} justify="flex-start">
-        <DetailsComponent data={data} />
-        <TabsComponent data={data} />
-      </Flex>
-    </Container>
+    <Box
+      h="100vw"
+      bgImg={data ? `url(${data?.background})` : null}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      py={10}
+    >
+      <Container maxW="container.xl" bg="whiteAlpha.500" h="100%" py={10}>
+        <Flex gap={6} justify="flex-start">
+          <DetailsComponent data={data} />
+          <TabsComponent data={data} />
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 
