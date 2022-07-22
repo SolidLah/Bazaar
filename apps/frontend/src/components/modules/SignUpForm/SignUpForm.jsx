510568becc8195c5f938d0482a29b20fc4eb0438
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-22 10:34:28 +0800

    [frontend] toastedCallback for SignUpForm

--- apps/frontend/src/components/modules/SignUpForm/SignUpForm.jsx
+++ apps/frontend/src/components/modules/SignUpForm/SignUpForm.jsx
@@ -1,60 +1,43 @@
 import { Button, Center, Flex, Heading, Input } from "@chakra-ui/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useContext, useRef } from "react";
-import LoadingLayout from "src/components/common/layouts/LoadingLayout";
-import { userContext } from "src/contexts/userContext";
-import { registerWithEmailAndPassword } from "src/lib/firebase";
-import useErrorToast from "src/lib/hooks/useErrorToast";
+import { useState } from "react";
+import PasswordInput from "src/components/common/ui/PasswordInput/PasswordInput";
+import WalletHandlerButton from "src/components/common/ui/WalletHandlerButton/WalletHandlerButton";
+import { signupWithEmailAndPassword } from "src/lib/firebase";
+import { useToastedCallback } from "src/lib/hooks";
+import useFetchUserFromWalletAddress from "src/lib/hooks/useFetchUserFromWalletAddress";
+import useEthersStore from "src/stores/ethersStore";
 
 const SignupForm = () => {
-  const emailRef = useRef();
-  const nameRef = useRef();
-  const passwordRef = useRef();
-  const confirmPasswordRef = useRef();
-  const { authState } = useContext(userContext);
-  const [user, loading] = authState;
-  const router = useRouter();
-  const errorToast = useErrorToast("Sign up");
+  const walletAddress = useEthersStore((state) => state.address);
+  const { data: addressInUse } = useFetchUserFromWalletAddress(walletAddress);
+  console.log(walletAddress, addressInUse);
 
-  if (user) {
-    router.push(`/user/${user.uid}`);
-  }
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
 
-  const register = async () => {
-    const name = nameRef.current?.value;
-    const email = emailRef.current?.value;
-    const password = passwordRef.current?.value;
-    const confirmPassword = confirmPasswordRef.current?.value;
+  const signup = async () => {
+    if (!email || !name || !password || !confirm || !walletAddress)
+      throw new Error("Missing fields");
+    if (password !== confirm) throw new Error("Passwords do not match");
+    if (addressInUse) throw new Error("Address is already used");
 
-    if (!name || !email || !password || !confirmPassword) {
-      errorToast({
-        description: "Missing fields",
-      });
+    await signupWithEmailAndPassword(name, email, password, walletAddress);
 
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      errorToast({
-        description: "Passwords do not match",
-      });
-
-      return;
-    }
-
-    try {
-      await registerWithEmailAndPassword(name, email, password);
-    } catch (error) {
-      errorToast({
-        description: error.message,
-      });
-    }
+    setEmail("");
+    setName("");
+    setPassword("");
+    setConfirm("");
   };
 
-  if (loading) {
-    return <LoadingLayout />;
-  }
+  const { toastedCallback, loading } = useToastedCallback("Sign up", signup);
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handleName = (e) => setName(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleConfirm = (e) => setConfirm(e.target.value);
 
   return (
     <Center mt={20}>
@@ -62,23 +45,42 @@ const SignupForm = () => {
         <Heading mb={6} align="center">
           Sign Up
         </Heading>
-        <Input ref={nameRef} placeholder="name" variant="filled" mb={3} />
-        <Input ref={emailRef} placeholder="email" variant="filled" mb={3} />
         <Input
-          ref={passwordRef}
+          value={name}
+          onChange={handleName}
+          placeholder="name"
+          variant="filled"
+          mb={3}
+        />
+        <Input
+          value={email}
+          onChange={handleEmail}
+          type="email"
+          placeholder="email"
+          variant="filled"
+          mb={3}
+        />
+        <PasswordInput
+          value={password}
+          onChange={handlePassword}
           placeholder="password"
           variant="filled"
           mb={3}
-          type="password"
         />
-        <Input
-          ref={confirmPasswordRef}
+        <PasswordInput
+          value={confirm}
+          onChange={handleConfirm}
           placeholder="confirm password"
           variant="filled"
-          mb={6}
-          type="password"
+          mb={3}
         />
-        <Button colorScheme="purple" mb={6} onClick={register}>
+        <WalletHandlerButton mb={6} bg="gray.300" />
+        <Button
+          colorScheme="purple"
+          mb={6}
+          onClick={toastedCallback}
+          isLoading={loading}
+        >
           Sign Up
         </Button>
         <Link href="/user/login" passHref>
